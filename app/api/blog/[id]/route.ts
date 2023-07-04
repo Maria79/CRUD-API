488ef04f933a83c4e93fcc264af691de58caf498
Maria79 <mariajalbisu@gmail.com>
2023-07-04 14:42:39 +0100

    now

---
+++ app/api/blog/[id]/route.ts
@@ -0,0 +1,13 @@
+export const GET = async (req: Request) => {
+	console.log('GET ITEM');
+};
+
+export const PUT = async (req: Request) => {
+	console.log('PUT Item');
+	// update a post by id
+};
+
+export const DELETE = async () => {
+	console.log('DELETE item');
+	// Delete a post by id
+};
--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,3 +1,7 @@
 export const GET = async (req: Request, res: Response) => {
 	console.log('GET REQUEST');
 };
+
+export const POST = async (req: Request, res: Response) => {
+	console.log('POST REQUEST');
+};
